f00468c22915d6a3d5aab694c7db95b75c305bb8
Sebastian Slutzky <sebastian.slutzky@wonga.com>
2012-07-03 11:49:59 +0100

    Added custom messagemapping impl

--- spykes/DynamicEndpoint/GenericEndpoint/EndpointConfigurator.cs
+++ spykes/DynamicEndpoint/GenericEndpoint/EndpointConfigurator.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Linq;
+using System.Transactions;
 using Common.Logging;
 using Autofac;
 using NServiceBus;
+using NServiceBus.Config.ConfigurationSource;
+using NServiceBus.ObjectBuilder;
+using NServiceBus.Serializers.XML;
+using NServiceBus.Unicast.Transport;
 using Wonga.Ops.Configuration;
 using Wonga.Ops.Configuration.Bus;
+using Wonga.Risk.Business;
 
 namespace GenericEndpoint
 {
@@ -13,13 +20,58 @@ namespace GenericEndpoint
 
 		public void InitialiseEndpoint()
 		{
+			//OpsBusConfiguration();
+			MyBusConfiguration();
+		}
+
+
+		private void OpsBusConfiguration()
+		{
 			IContainer container = base.Init();
 			//now we autosubscribe, need it to rewrite for Sagas - because need to write Dummy Sagas handlers
-			var bus = BusConfigurator.ConfigureEndpointBus(container, isAutoSubscribe: false,runTests:false);
+			var bus = BusConfigurator.ConfigureEndpointBus(container, isAutoSubscribe: false, runTests: false);
 			bus.CreateBus().Start();
 		}
 
-		
+		private static void MyBusConfiguration()
+		{
+			var builder = new ContainerBuilder();
+
+
+			builder.RegisterType<NServiceBusConfigSource>().As<IConfigurationSource>().PropertiesAutowired().
+				InstancePerLifetimeScope();
+			var container = builder.Build();
+
+
+			var anotherBuilder = new ContainerBuilder();
+			//anotherBuilder.RegisterModule<MySerializerModule>();
+			anotherBuilder.Update(container);
+
+			var busConfigSource = container.Resolve<IConfigurationSource>() as NServiceBusConfigSource;
+			
+			var bus = Configure.With(
+				typeof(IMessage).Assembly,
+				typeof(CompletionMessage).Assembly,
+				typeof(IBusinessApplicationAccepted).Assembly,
+				typeof(OnTheFlyHandler).Assembly
+				);
+			bus.CustomConfigurationSource(busConfigSource);
+			bus.Autofac2Builder(container);
+			ConfigureSerializer(bus);
+			bus.MsmqTransport()
+				.IsTransactional(true)
+				.IsolationLevel(IsolationLevel.RepeatableRead)
+				.PurgeOnStartup(false)
+				.UnicastBus()
+				.ImpersonateSender(true)
+				.LoadMessageHandlers()
+				.DoNotAutoSubscribe().CreateBus().Start();
+
+
+			//        .LoadMessageHandlers()
+			//    .CreateBus()
+			//    .Start();
+		}
 		public EndpointConfigurator(string endpointName)
 		{
 			EndpointName = endpointName;
@@ -31,6 +83,17 @@ namespace GenericEndpoint
 			//throw new NotImplementedException();
 		}
 
+		private static Configure ConfigureSerializer(Configure config)
+		{
+			var messageTypes = Configure.TypesToScan.Where(t => typeof(IMessage).IsAssignableFrom(t)).ToList();
+			var decoratedMapper = new NServiceBus.MessageInterfaces.MessageMapper.Reflection.MessageMapper();
+
+			config.Configurer.ConfigureComponent<NServiceBus.MessageInterfaces.MessageMapper.Reflection.MessageMapper>(ComponentCallModelEnum.Singleton);
+			config.Configurer.ConfigureComponent<MySerializer>(ComponentCallModelEnum.Singleton)
+				.ConfigureProperty(ms => ms.DecoratedSerializer, new MessageSerializer() { MessageTypes = messageTypes ,MessageMapper = new MyMessageMapper()});
+
+			return config;
+		}
 		
 	}
 }
---
+++ spykes/DynamicEndpoint/GenericEndpoint/MySerializer.cs
@@ -0,0 +1,56 @@
+﻿
+using NServiceBus.MessageInterfaces;
+using NServiceBus.Serialization;
+using NServiceBus.Serializers.XML;
+
+namespace GenericEndpoint
+{
+	public class MyMessageMapper:IMessageMapper{
+
+		public System.Type GetMappedTypeFor(string typeName)
+		{
+
+			return System.Type.GetType(typeName);
+		}
+
+		public System.Type GetMappedTypeFor(System.Type t)
+		{
+			return t;
+		}
+
+		public void Initialize(System.Collections.Generic.IEnumerable<System.Type> types)
+		{
+			//throw new System.NotImplementedException();
+		}
+
+		public object CreateInstance(System.Type messageType)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public T CreateInstance<T>(System.Action<T> action) where T : NServiceBus.IMessage
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public T CreateInstance<T>() where T : NServiceBus.IMessage
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+
+	public class MySerializer : IMessageSerializer
+	{
+		public MessageSerializer DecoratedSerializer { get; set; }
+
+		public NServiceBus.IMessage[] Deserialize(System.IO.Stream stream)
+		{
+			return DecoratedSerializer.Deserialize(stream);
+		}
+
+		public void Serialize(NServiceBus.IMessage[] messages, System.IO.Stream stream)
+		{
+			DecoratedSerializer.Serialize(messages, stream);
+		}
+	}
+}
